2eaf874a9b83752a1ffbfaa5b965bc3a085816e2
Wissem BEN CHAABANE <benchaaben.wissem@gmail.com>
2022-12-15 22:29:30 +0000

    feat: add entrypoint

--- .devcontainer/post-start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cat ~/.gitconfig |grep '\[safe\]' > /dev/null 2>&1 || printf '[safe]\n\tdirectory = /workspaces/dev-tools' >> ~/.gitconfig
--- src/devtools/install.sh
+++ src/devtools/install.sh
@@ -26,19 +26,10 @@ export HISTFILE=/dc/shellhistory/.zsh_history
 export PROMPT_COMMAND='history -a'
 sudo chown -R $_REMOTE_USER /dc/shellhistory
 EOF
-chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.zshrc
-
-# Fix zsh shellhistory
-if [ -f /dc/shellhistory/.zsh_history ]
-then
-    mv /dc/shellhistory/.zsh_history /dc/shellhistory/.zsh_history_bad
-    strings -eS /dc/shellhistory/.zsh_history_bad > /dc/shellhistory/.zsh_history
-    fc -r /dc/shellhistory/.zsh_history
-    rm -f /dc/shellhistory/.zsh_history_bad
-fi
+chown -R "$_REMOTE_USER" "$_REMOTE_USER_HOME"/.zshrc
 
 # Install FZF
 if [[ ! -d $_REMOTE_USER_HOME/.fzf ]]
 then
-    su $_REMOTE_USER -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all"
+    su "$_REMOTE_USER" -c "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all"
 fi
